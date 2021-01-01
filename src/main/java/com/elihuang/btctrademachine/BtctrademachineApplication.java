404cba299a9de5a2af0ef6c81944884f4c2a24e1
elimacbook <elihuang@elimacbookdeMacBook-Pro.local>
2021-01-01 14:22:20 +0800

    first

---
+++ src/main/java/com/elihuang/btctrademachine/BtctrademachineApplication.java
@@ -0,0 +1,13 @@
+package com.elihuang.btctrademachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtctrademachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtctrademachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elihuang/btctrademachine/BtctrademachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elihuang.btctrademachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtctrademachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
